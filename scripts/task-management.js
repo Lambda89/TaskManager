f7cfed978b8efda26462d57bbd3fb2753bcce13c
Rickard Lund <natural_blindfold@hotmail.com>
2011-03-29 21:03:27 +0200

    Adds a first JavaScript-file to project, and scripts/ folder.

---
+++ scripts/task-management.js
@@ -0,0 +1,28 @@
+/**
+
+Author: Rickard Lund
+Created: 2011-03-29
+Purpose: Handle Javascripts for task-entities.
+Comment:
+	Saves/reloads data and transforms visual elements of task-entities
+	Currently a "placeholder" to show off expected "style" of code,
+	and or functionality
+	
+Uses:
+	jQuery Javascript Library - /jquery/jquery.js
+	jQuery User Interface Library - /jquery/jquery-ui.js
+
+**/
+
+$(document).ready(function(){
+	class TaskManagement {
+	
+		public TaskManagement() {
+			this.loadTasks();
+		}
+		
+		private function loadTasks() {
+			
+		}
+	}
+});
