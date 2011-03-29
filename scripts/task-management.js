3094310ba4c983a2245fee777ccc26912d1a450b
Rickard Lund <natural_blindfold@hotmail.com>
2011-03-29 21:31:26 +0200

    Changed description to fit with intent of script-file.

--- scripts/task-management.js
+++ scripts/task-management.js
@@ -4,7 +4,7 @@ Author: Rickard Lund
 Created: 2011-03-29
 Purpose: Handle Javascripts for task-entities.
 Comment:
-	Saves/reloads data and transforms visual elements of task-entities
+	Sends requests to manipulate data of task-entities.
 	Currently a "placeholder" to show off expected "style" of code,
 	and or functionality
 	
@@ -16,14 +16,18 @@ Uses:
 
 $(document).ready(function(){
 	
-	//On-click-events
+	/**
+	Event-handling
+	**/
 	
 	$('#element').click(function() { loadTasks(); } );
 	$('#save_element').click(function() { saveTasks(); } );
 	$('#remove_element').click(function() { removeTask(); } );
 	
-	// Functions
-	
+	/**
+	Functions
+	**/
+
 	function loadTasks() {
 		
 	}
