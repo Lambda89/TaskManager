3218ce94491209f0054f8d940c1f890e51780cf4
Werner Johansson <tux@linux-Elysium2.site>
2011-04-02 22:17:17 +0200

    Added commentary to ensure that the presense and purpose of this file is apparent later.

--- System/index.php
+++ System/index.php
@@ -1,4 +1,19 @@
 <?php
+	/**
+		<?php
+	/**
+		Authour: Werner Johansson
+		Created: 2011-04-02
+		Purpose: To ensure none can get a read on this directory.
+		Comment:
+		
+		@package 
+		uses:
+			n/a - n/a
+			  
+	**/
+
  	header('Location: ../index.php');
 	die(); 
+
 ?>
