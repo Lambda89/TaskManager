02877efc51a1b3085dfda2d9cc1b7a41daef7641
Werner Johansson <tux@linux-Elysium2.site>
2011-04-02 22:22:09 +0200

    Added a note to discuss.

--- index.php
+++ index.php
@@ -2,6 +2,15 @@
 <!DOCTYPE html>
 <!-- HTML-standard not yet selected. -->
 
+<?php 
+/**
+	Don't get mad but this is a generally not a good way. Where is the pre-processing
+	of the request? sys is a dead class so there is none there? Where is the check
+	for what browser etc being used?
+	I think we need to redo this approach a little or we'll end up with a massive
+	refactor down the road.
+**/ 
+?>
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
