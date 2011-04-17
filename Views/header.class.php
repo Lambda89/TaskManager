372d3714309ba6c8ba17ec4dabd7e6f3b60ae388
Rickard Lund <natural_blindfold@hotmail.com>
2011-04-17 23:00:31 +0200

    Some fixing.

---
+++ Views/header.class.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+* 
+*/
+class Header
+{
+	public $head;
+	
+	private $page = '';
+	private $doctype = '';
+	private $meta = '';
+	private $title = '';
+	private $javascript_paths = array();
+	private $css_paths = array();
+
+	static private $jquery_paths = array('jquery.js', 'jquery-ui.js');
+
+	function __construct($page)
+	{
+		$this->setDoctype();
+		$this->setMeta($this->meta);
+		$this->setTitle($this->title);
+	}
+}
+
+
+?>
