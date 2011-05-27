a7d7c4f2911463fc4571568448a10e5576abcbd1
Werner Johansson <tux@linux-Elysium2.site>
2011-05-28 00:55:59 +0200

    A new DB schema, a few changes to reflect the new code.

---
+++ Docs/SQL/sql_db_create_110527.sql
@@ -0,0 +1,48 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+
+DROP SCHEMA IF EXISTS `TaskManager` ;
+CREATE SCHEMA IF NOT EXISTS `TaskManager` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
+USE `TaskManager` ;
+
+-- -----------------------------------------------------
+-- Table `TaskManager`.`user_contact_data_entity`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `TaskManager`.`user_contact_data_entity` ;
+
+CREATE  TABLE IF NOT EXISTS `TaskManager`.`user_contact_data_entity` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Unsigned int ID that autoincs as more are added.' ,
+  `fk_user_email` VARCHAR(100) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+  `type` ENUM('PROTOCOL','BIODATA','OTHER') CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+  `identifier` VARCHAR(15) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+  `data` VARCHAR(512) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin
+COMMENT = 'This represents one contact data point about one user.';
+
+
+-- -----------------------------------------------------
+-- Table `TaskManager`.`user_entity`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `TaskManager`.`user_entity` ;
+
+CREATE  TABLE IF NOT EXISTS `TaskManager`.`user_entity` (
+  `email` VARCHAR(100) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL COMMENT 'The preferred way to contact this user. Also works as a login.' ,
+  `login` VARCHAR(25) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL COMMENT 'The name the user are shown them selves or others.' ,
+  `passwd` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL COMMENT 'Hashed with either SHA1 or MD5 encoding.' ,
+  `createdAt` DATETIME NOT NULL COMMENT 'When this entity was created. It\'s part since we might want to track this as well as use it for various salts.' ,
+  `status` ENUM('ACTIVE','INACTIVE','BANNED','REGISTERED','NOUSER') NOT NULL ,
+  PRIMARY KEY (`email`) )
+ENGINE = MyISAM
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin
+COMMENT = 'Defines a user entity in the TaskManager application';
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
